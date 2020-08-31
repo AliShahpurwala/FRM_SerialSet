6bfd2656b4a88a40b3906c7fc4d099101584b68a
Ali Shahpurwala <57410691+AliShahpurwala@users.noreply.github.com>
2020-08-31 15:07:19 -0400

    Completed Visual Aspect of the prompt box.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,11 +30,15 @@
         {
             this.NameInput = new System.Windows.Forms.TextBox();
             this.OK = new System.Windows.Forms.Button();
+            this.readLocationLabel = new System.Windows.Forms.Label();
+            this.readDataRangeLabel = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.connectButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // NameInput
             // 
-            this.NameInput.Location = new System.Drawing.Point(41, 34);
+            this.NameInput.Location = new System.Drawing.Point(141, 39);
             this.NameInput.Name = "NameInput";
             this.NameInput.Size = new System.Drawing.Size(149, 20);
             this.NameInput.TabIndex = 0;
@@ -49,15 +53,55 @@
             this.OK.UseVisualStyleBackColor = true;
             this.OK.Click += new System.EventHandler(this.OK_Click);
             // 
+            // readLocationLabel
+            // 
+            this.readLocationLabel.AutoSize = true;
+            this.readLocationLabel.Location = new System.Drawing.Point(41, 42);
+            this.readLocationLabel.Name = "readLocationLabel";
+            this.readLocationLabel.Size = new System.Drawing.Size(77, 13);
+            this.readLocationLabel.TabIndex = 2;
+            this.readLocationLabel.Text = "Read Location";
+            this.readLocationLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // readDataRangeLabel
+            // 
+            this.readDataRangeLabel.AutoSize = true;
+            this.readDataRangeLabel.Location = new System.Drawing.Point(41, 83);
+            this.readDataRangeLabel.Name = "readDataRangeLabel";
+            this.readDataRangeLabel.Size = new System.Drawing.Size(94, 13);
+            this.readDataRangeLabel.TabIndex = 3;
+            this.readDataRangeLabel.Text = "Read Data Range";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(141, 79);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(149, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // connectButton
+            // 
+            this.connectButton.Location = new System.Drawing.Point(567, 33);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(128, 31);
+            this.connectButton.TabIndex = 5;
+            this.connectButton.Text = "Connect";
+            this.connectButton.UseVisualStyleBackColor = true;
+            this.connectButton.Click += new System.EventHandler(this.connectButton_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.connectButton);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.readDataRangeLabel);
+            this.Controls.Add(this.readLocationLabel);
             this.Controls.Add(this.OK);
             this.Controls.Add(this.NameInput);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Serial Connection";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -67,6 +111,10 @@
 
         private System.Windows.Forms.TextBox NameInput;
         private System.Windows.Forms.Button OK;
+        private System.Windows.Forms.Label readLocationLabel;
+        private System.Windows.Forms.Label readDataRangeLabel;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button connectButton;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -10,8 +10,10 @@ using System.Windows.Forms;
 
 namespace FRM_SerialSet
 {
+    
     public partial class Form1 : Form
     {
+        connectionStatus ConnectionStatus = new connectionStatus();
         public Form1()
         {
             InitializeComponent();
@@ -22,5 +24,257 @@ namespace FRM_SerialSet
             String givenName = this.NameInput.Text;
             MessageBox.Show(givenName);
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Enter the start read location in the textbox");
+        }
+
+        private void connectButton_Click(object sender, EventArgs e)
+        {
+            Form connectionDetails = new Form();
+            connectionDetails.Height = 300;
+            connectionDetails.Width = 300;
+            connectionDetails.Text = "Connection Details";
+            
+            Label comportInputLabel = new Label() { Top = 20, Left = 20, Text = "Comport" };
+            TextBox comportInput = new TextBox() { Top = 20, Left = 120};
+
+            Label baudInputLabel = new Label() { Top = 50, Left = 20, Text = "Baud" };
+            TextBox baudInput = new TextBox() { Top = 50, Left = 120 };
+
+            Label parityInputLabel = new Label() { Top = 80, Left = 20, Text = "Parity" };
+            TextBox parityInput = new TextBox() { Top = 80, Left = 120 };
+
+            Label stopBitInputLabel = new Label() { Top = 110, Left = 20, Text = "Stop Bit" };
+            TextBox stopBitInput = new TextBox() { Top = 110, Left = 120 };
+
+            Button promptOK = new Button() { Text = "OK", Top = 200, Left = 120 };
+            Button promptCancel = new Button() { Text = "Cancel", Top = 200, Left = 200 };
+            
+            
+            promptOK.Click += (sender1, e1) => {
+                connectionDetails.Close();
+            };
+            promptCancel.Click += (sender1, e1) => {
+                connectionDetails.Close();
+            };
+            connectionDetails.Controls.Add(comportInputLabel);
+            connectionDetails.Controls.Add(comportInput);
+            connectionDetails.Controls.Add(baudInput);
+            connectionDetails.Controls.Add(baudInputLabel);
+            connectionDetails.Controls.Add(parityInput);
+            connectionDetails.Controls.Add(parityInputLabel);
+            connectionDetails.Controls.Add(stopBitInputLabel);
+            connectionDetails.Controls.Add(stopBitInput);
+            connectionDetails.Controls.Add(promptOK);
+            connectionDetails.Controls.Add(promptCancel);
+            connectionDetails.ShowDialog();
+
+            
+            
+            
+        }
     }
+
+    public class connectionStatus
+    {
+        bool status;
+
+        public connectionStatus()
+        {
+            this.status = false;
+        }
+
+        private void changeStatus()
+        {
+            if (this.status == false)
+            {
+                this.status = true;
+            }
+            else
+            {
+                this.status = false;
+            }
+        }
+    }
+
+    #region Serial Communication Class - User Defined
+    class SerialComm
+    {
+        private char startKey = '@';
+        private string nodeNum = "00";
+        private string headerCode = "RD";
+        private string startPosition;
+        private string positionCount;
+        private string FCS;
+        private string endKey = "*\n";
+        private bool fuse = false;
+        public bool Fuse
+        {
+            get { return fuse; }
+        }
+
+        public SerialComm(string startPosition, string positionCount)
+        {
+            if (validatePosition(fillPosition(startPosition)))
+            {
+                this.startPosition = fillPosition(startPosition);
+            }
+            else
+            {
+                this.fuse = true;
+            }
+            if (validatePosition(fillPosition(positionCount)))
+            {
+                this.positionCount = fillPosition(positionCount);
+            }
+            else
+            {
+                this.fuse = true;
+            }
+            if (!this.fuse)
+            {
+                if (outOfBounds(Int32.Parse(this.startPosition), Int32.Parse(this.positionCount)))
+                {
+                    this.fuse = true;
+                }
+            }
+            if (!this.fuse)
+            {
+                this.FCS = fcsCalculate();
+            }
+
+
+        }
+
+        private string fcsCalculate()
+        {
+            string eval = this.startKey + this.nodeNum + this.headerCode + this.startPosition + this.positionCount;
+            char[] evalArray = eval.ToCharArray();
+            bool[] result = new bool[8];
+            bool[] compare = new bool[8];
+            foreach (char ch in evalArray)
+            {
+                compare = charToBin(ch);
+                for (int r = 0; r < 8; r++)
+                {
+                    result[r] = result[r] ^ compare[r];
+                }
+            }
+            return binToChar(result);
+        }
+        private string binToChar(bool[] input)
+        {
+            double result = 0;
+            double result2 = 0;
+            bool[] firstSet = new bool[4];
+            bool[] secondSet = new bool[4];
+            for (int i = 0; i < 4; i++)
+            {
+                firstSet[i] = input[i];
+            }
+            for (int y = 0; y < 4; y++)
+            {
+                secondSet[y] = input[y + 4];
+            }
+            for (int t = 0; t < 4; t++)
+            {
+                if (firstSet[t])
+                {
+                    result += Math.Pow(2, 3 - t);
+                }
+                if (secondSet[t])
+                {
+                    result2 += Math.Pow(2, 3 - t);
+                }
+            }
+            string final = result.ToString() + result2.ToString();
+            return final;
+        }
+        private bool[] charToBin(char c)
+        {
+            bool[] result = new bool[8];
+            int charCode = (int)c;
+            string binaryValue = fillBinary(Convert.ToString(charCode, 2));
+            char[] binaryValueArray = binaryValue.ToCharArray();
+            for (int y = 0; y < binaryValueArray.Length; y++)
+            {
+                if (binaryValueArray[y] == '1')
+                {
+                    result[y] = true;
+                }
+                else
+                {
+                    result[y] = false;
+                }
+            }
+            return result;
+        }
+        private string fillBinary(string s)
+        {
+            int numEmpty = 8 - s.Length;
+            for (int i = 0; i < numEmpty; i++)
+            {
+                s = "0" + s;
+            }
+            return s;
+        }
+        public string result()
+        {
+            if (this.fuse)
+            {
+                return "Object was not created succesfully";
+            }
+            else
+            {
+                return this.startKey + this.nodeNum + this.headerCode + this.startPosition + this.positionCount + this.FCS + this.endKey;
+            }
+        }
+        private bool outOfBounds(int startPos, int endPos)
+        {
+            if (startPos + endPos > 9999)
+            {
+                return true;
+            }
+            return false;
+        }
+        private bool validatePosition(string position)
+        {
+            if (position.Length > 4)
+            {
+                return false;
+            }
+            try
+            {
+                Int32.Parse(position);
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+        private string fillPosition(string position)
+        {
+            if (position.Length == 1)
+            {
+                return "000" + position;
+            }
+            else if (position.Length == 2)
+            {
+                return "00" + position;
+            }
+            else if (position.Length == 3)
+            {
+                return "0" + position;
+            }
+            else
+            {
+                return position;
+            }
+
+        }
+    }
+    #endregion
 }
